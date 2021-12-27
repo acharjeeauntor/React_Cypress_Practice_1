46ac5bec88a9378301b9477fcbc834f787833d13
Auntor Acharja <acharjeeauntor@gmail.com>
2021-12-27 23:28:17 +0600

    Setup Environment

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+// use for autocompletion
+
+describe('Locators Practice',()=>{
+    beforeEach(()=>{
+        cy.visit('/elements')
+    })
+
+    it('Locating Element with get',()=>{
+        
+    })
+})
